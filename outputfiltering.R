ee8924890dfcc00adf052154a85823a79b975e9f
CQuin042 <78384584+CQuin042@users.noreply.github.com>
2024-06-05 17:10:17 -0400

    changes

--- outputfiltering.R
+++ outputfiltering.R
@@ -4,3 +4,5 @@ patch.streamflow + hill.gw.Qout
 p <- build_output_filter(timestep = "daily", output_format= "csv", output_path = "/test", output_filename = "testbasinstreamflow", spatial_level = "basin",spatial_ID = "1", variables = "base_flow" )
 
 p
+
+
